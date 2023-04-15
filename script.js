6131a35dde66eb1de21a78c215a3403d1beb1c09
Siddharth Hota <80689111+Bluetooth-stack@users.noreply.github.com>
2023-04-15 23:25:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+ function getSize(){
+      let div = document.querySelector('div>h1');
+      let h = window.innerHeight;
+      let w = window.innerWidth;
+      
+      div.textContent = "Width: " + w + " and Height: " + h
+      // alert("loded");
+    }
